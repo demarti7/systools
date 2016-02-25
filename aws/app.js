91f8831449e7a4c1bf2d9d666ab13c25c3f46ea2
jayoharedee <shme@protonmail.com>
2016-02-24 21:07:31 -0500

    changed to use method using filter search

--- aws/app.js
+++ aws/app.js
@@ -1,14 +1,8 @@
 const commander = require('commander')
+const update    = require('./update')
 const aws       = require('aws-sdk')
-const ec2       = require('./ec2')(aws)
 
-const accessKeyId = process.env.AWS_KEY
-const secretKey   = process.env.AWS_SECRET
+aws.config.update(update.keyConfig)
+aws.config.update(update.region)
 
-const keyConfig = {
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretKey
-}
-
-aws.config.update(keyConfig)
-aws.config.update({region: 'use-west-2'})
+module.exports = aws
